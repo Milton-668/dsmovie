eb8d2726771e1fe2765e147ce29dcaaece06e0c1
Milton <miltonmjcp@gmail.com>
2022-03-13 21:50:31 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,28 @@
-import axios from "axios";
-import MovieCard from "components/MovieCard";
-import Pagination from "components/pagination";
-import { BASE_URL } from "utils/request";
+import axios from 'axios';
+import MovieCard from 'components/MovieCard';
+import Pagination from 'components/pagination';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
+import { BASE_URL } from 'utils/request';
 
 function Listing() {
 
-  //Forma incorreta
-  axios.get(`${BASE_URL}/movies?size=10&page=0`)
-  .then( response =>{
-    console.log(response.data);
-  });
+  const [pageNumber, setPageNumber] = useState(0);
+  
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=10&page=1`)
+    .then( response =>{
+      // Converte o data em MoviePage ou seja está aterelado a página passada via URL
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     //fragment -> simula uma div não interfere em nada
     <>
+    <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
